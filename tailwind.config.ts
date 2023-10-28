e16de8bc2d365743f0b2ae17d6dd437e4d71c739
Steven Le <stevenleusa79@yahoo.com>
2023-10-28 16:29:16 -0700

    finished most of navbar, add styling to login page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './public/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -12,6 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      fontFamily: {
+        lato: ['Lato'],
       },
     },
   },
