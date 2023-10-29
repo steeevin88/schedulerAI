79c543e756ac01b3f55662c8af572af0fb51d63d
Steven Le <stevenleusa79@yahoo.com>
2023-10-28 22:27:27 -0700

    redesigned signup page to match login

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -13,6 +13,9 @@ const Page = () => {
     icsUrl: '',
     preferences: '',
   })
+
+  const { email, password } = signUpInfo;
+
   function handleChange(e: any) {
     setSignUpInfo((prevState) => ({
         ...prevState,
@@ -51,17 +54,30 @@ const Page = () => {
   }
 
   return (
-    <div className='w-full h-[80vh] justify-center content-centers flex flex-grow'>
-      <form className='rounded mx-[10%] mt-[10%]  p-5 flex flex-col bg-blue-50 flex-grow'>
-          <input className='p-2 my-2 rounded bg-blue-100' name="email" type="email" placeholder="Email" onChange={handleChange} />
-          <input className='p-2 my-2 rounded bg-blue-100' name="password" type="password" placeholder="Password" onChange={handleChange} />
-          <input className='p-2 my-2 rounded bg-blue-100' name="confirmPassword" type="password" placeholder="Confirm Password" onChange={handleChange} />
-          <input className='p-2 my-2 rounded bg-blue-100' name="icsUrl" type="text" placeholder="Link to ICS file" onChange={handleChange} />
-          <input className='p-2 my-2 rounded bg-blue-100' name="preferences" type="text" placeholder="What type of events do you enjoy? ('Clubbing, sports, dancing, etc')" onChange={handleChange} />
-          <button className='bg-blue-500 rounded p-5' onClick={handleClick} >Sign Up</button>
-          {error && (errorLink ? <Link href={errorLink} className='underline'>{error}. Click here to Log In</Link>: <p className='text-red-500'>{error}</p>)}
+    <section className='text-center'>
+    <h1 className="my-7 text-7xl font-lato">Sign Up</h1>
+      <form className='w-full justify-center content-centers flex flex-col'>
+        <div>
+          <input className='p-5 bg-blue-100 w-[50%] h-[5vh] my-3 border' name="email" type='email' placeholder="Email" value={email} onChange={handleChange} />
+        </div>
+        <div>
+          <input className='p-5 bg-blue-100 w-[50%] h-[5vh] my-3 border' name="password" type="password" placeholder="Password" value={password} onChange={handleChange} />
+        </div>
+        <div>
+          <input className='p-5 bg-blue-100 w-[50%] h-[5vh] my-3 border' name="confirmPassword" type="password" placeholder="Confirm Password" onChange={handleChange} />
+        </div>
+        <div>
+          <input className='p-5 bg-blue-100 w-[50%] h-[5vh] my-3 border' name="icsUrl" type="text" placeholder="Link to ICS file" onChange={handleChange} />
+        </div>
+        <div>
+          <input className='p-5 bg-blue-100 w-[50%] h-[5vh] my-3 border' name="preferences" type="text" placeholder="What type of events do you enjoy? ('Clubbing, sports, dancing, etc')" onChange={handleChange} />
+        </div>
+        <div>
+          <button className='p-5 bg-blue-100 w-[50%] my-3 border rounded-xl' onClick={handleClick}>Sign Up</button>
+          {error && (errorLink ? <div><Link href={errorLink} className='underline'>{error}. Click here to Log In</Link></div>: <p className='text-red-500'>{error}</p>)} 
+        </div>
       </form>
-    </div>
+    </section>
   )
 }
 
